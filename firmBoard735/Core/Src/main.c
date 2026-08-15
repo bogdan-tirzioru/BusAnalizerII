efceebd95875fcd3e188c874671aa390860cf035
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-15 22:02:07 +0300

    test FDCAN1 nominal SJW 2

--- firmBoard735/Core/Src/main.c
+++ firmBoard735/Core/Src/main.c
@@ -298,7 +298,7 @@ static void MX_FDCAN1_Init(void)
   hfdcan1.Init.TransmitPause = DISABLE;
   hfdcan1.Init.ProtocolException = DISABLE;
   hfdcan1.Init.NominalPrescaler = 1;
-  hfdcan1.Init.NominalSyncJumpWidth = 1;
+  hfdcan1.Init.NominalSyncJumpWidth = 2;
   hfdcan1.Init.NominalTimeSeg1 = 13;
   hfdcan1.Init.NominalTimeSeg2 = 2;
   hfdcan1.Init.DataPrescaler = 1;
