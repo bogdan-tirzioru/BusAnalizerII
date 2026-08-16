c1e8a31c35f158d65979e1b41ada9f36c7972344
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-16 08:26:13 +0300

    Add binary protocol self-test interface

---
+++ firmBoard735/Core/Inc/baii_protocol_selftest.h
@@ -0,0 +1,21 @@
+#ifndef BAII_PROTOCOL_SELFTEST_H
+#define BAII_PROTOCOL_SELFTEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Temporary bring-up test for BusAnalyzerII host protocol v0.1.
+ *
+ * Tests GET_INFO, GET/SET_RTC_TIME and CAN bitrate reconfiguration before
+ * USB transport is introduced. The hardware portions expect an external CAN
+ * generator as described by the console prompts.
+ */
+void BAII_Protocol_SelfTest(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BAII_PROTOCOL_SELFTEST_H */
